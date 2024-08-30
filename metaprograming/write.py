288de21738e4b326e6ffdcadea5f23255862d6b0
Juan Carlos Aranda Huecas <juanki0396@gmail.com>
2024-08-30 19:12:52 +0200

    Improved metaprogramming to generate MCParticle file

--- metaprograming/write.py
+++ metaprograming/write.py
@@ -10,13 +10,23 @@ def add_includes(file: TextIO, includes: list[str]) -> None:
 def rvec(t: str) -> str:
     return f"ROOT::VecOps::RVec<{t}>"
 
-def momentum_property(inst_name: str, p_m_name: str, prop_name: str, var_name:str) -> str:
-    return (f"float px = {inst_name}.{p_m_name}.x;\n"
-            f"float py = {inst_name}.{p_m_name}.y;\n"
-            f"float pz = {inst_name}.{p_m_name}.z;\n"
-            f"float m = {inst_name}.mass;\n"
+def momentum_property(inst_name: str, p_m_name: str, prop_name: str, var_name:str, float_t: str = "float") -> str:
+    return (f"{float_t} px = {inst_name}.{p_m_name}.x;\n"
+            f"{float_t} py = {inst_name}.{p_m_name}.y;\n"
+            f"{float_t} pz = {inst_name}.{p_m_name}.z;\n"
+            f"{float_t} m = {inst_name}.mass;\n"
             f"ROOT::Math::PxPyPzMVector p(px,py,pz,m);\n"
             f"{var_name} = p.{prop_name}();\n")
+
+def vertex_property(inst_name: str, r_m_name: str, prop_name: str, var_name:str, float_t: str = "float") -> str:
+    return (f"{float_t} x = {inst_name}.{r_m_name}.x;\n"
+            f"{float_t} y = {inst_name}.{r_m_name}.y;\n"
+            f"{float_t} z = {inst_name}.{r_m_name}.z;\n"
+            f"ROOT::Math::XYZVector r(x,y,z);\n"
+            f"{var_name} = r.{prop_name}();\n")
+
+def obj_member(inst_name: str, obj_mem: str, var_name:str, aux_func: str = "") -> str:
+    return f"{var_name} = {aux_func}({inst_name}.{obj_mem});\n"
 
 class CppTypes(Enum):
     INT = auto()
@@ -84,7 +94,7 @@ class FunctionGetter(FunctionSignature):
     def __init__(self, name: str, out_t: str, edm_t: str, 
                  col_t: Edm4hepCollection, doc_property: str, 
                  get_code:str) -> None:
-        name = f"get_{name}"
+        func_name = f"get_{name}"
         vec_t = rvec(out_t)
         doc = f"Get {doc_property} from each item in a collection of {edm_t}."
         params = []
@@ -99,15 +109,88 @@ class FunctionGetter(FunctionSignature):
                 code = (
                     f"{vec_t} vec;\n"
                     f"vec.reserve(collection.size());\n"
-                    f"for(const edm4hep::{edm_t}Data x : collection)\u007b\n"
-                    f"{out_t} restult;") + get_code + (
+                    f"for(const edm4hep::{edm_t}Data &item : collection)\u007b\n"
+                    f"{out_t} result;\n") + get_code + (
                     f"vec.emplace_back(result);\n"
-                    f"  \u007d\n"
-                    f"  return result;\n")
+                    f"\u007d\n"
+                    f"return vec;\n")
             case Edm4hepCollection.COLLECTION:
                 raise NotImplementedError()
 
-        super().__init__(name, vec_t, params, doc, code)
+        super().__init__(func_name, vec_t, params, doc, code)
+
+class FunctionMask(FunctionSignature):
+    def __init__(self, name: str, val_t: str, edm_t: str, 
+                 col_t: Edm4hepCollection, doc_property: str) -> None:
+        func_name = f"mask_{name}"
+        vec_t = rvec("bool")
+        doc = f"Create boolean mask based on the value of {doc_property} from each item in a collection of {edm_t}."
+        params = []
+        code = ""
+        match col_t:
+            case Edm4hepCollection.RVEC:
+                params = [ 
+                    FunctionParameter("op", "LogicalOperators::ComparisonOperator",
+                                      f"Logical operation applied to create the mask."),
+                    FunctionParameter("val", val_t,
+                                      f"Value to compare with."),
+                    FunctionParameter("collection", rvec(f"edm4hep::{edm_t}Data"),
+                                      f"Collection of {edm_t} to look in")
+                ]
+                code = (
+                    f"{vec_t} vec;\n"
+                    f"vec.reserve(collection.size());\n"
+                    f"{rvec(val_t)} vals = get_{name}(collection);\n"
+                    f"for(const {val_t} &item : vals)\u007b\n"
+                    f"switch (op)\u007b\n"
+                    f"case LogicalOperators::ComparisonOperator::LESS:\n"
+                    f"vec.emplace_back(item < val);\n"
+                    f"break;\n"
+                    f"case LogicalOperators::ComparisonOperator::LESSEQ:\n"
+                    f"vec.emplace_back(item <= val);\n"
+                    f"break;\n"
+                    f"case LogicalOperators::ComparisonOperator::EQ:\n"
+                    f"vec.emplace_back(item == val);\n"
+                    f"break;\n"
+                    f"case LogicalOperators::ComparisonOperator::GREATEREQ:\n"
+                    f"vec.emplace_back(item >= val);\n"
+                    f"break;\n"
+                    f"case LogicalOperators::ComparisonOperator::GREATER:\n"
+                    f"vec.emplace_back(item > val);\n"
+                    f"break;\n"
+                    f"\u007d\n"
+                    f"\u007d\n"
+                    f"return vec;\n")
+            case Edm4hepCollection.COLLECTION:
+                raise NotImplementedError()
+
+        super().__init__(func_name, vec_t, params, doc, code)
+
+class FunctionSelector(FunctionSignature):
+    def __init__(self, name: str, val_t: str, edm_t: str, 
+                 col_t: Edm4hepCollection, doc_property: str) -> None:
+        func_name = f"sel_{name}"
+        vec_t = rvec(f"edm4hep::{edm_t}Data")
+        doc = f"Select a subgroup of {edm_t} based on the value of {doc_property}."
+        params = []
+        code = ""
+        match col_t:
+            case Edm4hepCollection.RVEC:
+                params = [ 
+                    FunctionParameter("op", "LogicalOperators::ComparisonOperator",
+                                      f"Logical operation applied to create the mask."),
+                    FunctionParameter("val", val_t,
+                                      f"Value to compare with."),
+                    FunctionParameter("collection", rvec(f"edm4hep::{edm_t}Data"),
+                                      f"Collection of {edm_t} to look in")
+                ]
+                code = (
+                    f"auto mask = mask_{name}(op, val, collection);\n"
+                    f"return LogicalOperators::filter(mask, collection);\n")
+            case Edm4hepCollection.COLLECTION:
+                raise NotImplementedError()
+
+        super().__init__(func_name, vec_t, params, doc, code)
 
 class RalClassWriter:
 
@@ -118,7 +201,6 @@ class RalClassWriter:
         self.includes = []
         self.namespaces = []
         self.functions = []
-        self.__initialize = False
     
     def add_edm_class(self, edm_class: str) -> None:
         self.edm_class = edm_class
@@ -138,11 +220,16 @@ class RalClassWriter:
                                 get_code)
         self.functions.append(getter)
 
-    def initialize(self) -> None:
-        self.__initialize = self.edm_class != "" and len(self.functions) > 0
-        if not self.__initialize:
-            raise RuntimeError("Failed initialization")
- 
+    def add_mask(self, name: str, val_t: str, col_t: Edm4hepCollection, 
+                 doc_property: str) -> None:
+        mask = FunctionMask(name, val_t, self.edm_class, col_t, doc_property)
+        self.functions.append(mask)
+
+    def add_selector(self, name: str, val_t: str, col_t: Edm4hepCollection, 
+                 doc_property: str) -> None:
+        mask = FunctionSelector(name, val_t, self.edm_class, col_t, doc_property)
+        self.functions.append(mask)
+
     def write(self, h_file: str, src_file: str) -> None:
         with open(self.include_dir / h_file, "w") as header, open(self.src_dir / src_file, "w") as source:
             h_code = [f"#include <{i}>\n" for i in self.includes]
@@ -151,25 +238,105 @@ class RalClassWriter:
             h_code.extend(["\u007d\n" for _ in self.namespaces])
             header.write("".join(h_code))
 
-            s_code = [f"#include <ral/{h_file}>"]
+            s_code = [f"#include <ral/{h_file}>\n"]
             s_code.extend([f"namespace {n}\u007b\n" for n in self.namespaces])
             s_code.extend([f.get_source() for f in self.functions])
             s_code.extend(["\u007d\n" for _ in self.namespaces])
             source.write("".join(s_code))
 
 if __name__ == "__main__":
-    writer = RalClassWriter("/tmp", "/tmp")
-    writer.add_edm_class("ReconstructedParticle")
+    writer = RalClassWriter("include/ral", "src")
+    writer.add_edm_class("MCParticle")
+
     writer.add_include("cmath")
     writer.add_include("Math/Vector3D.h")
     writer.add_include("Math/Vector4D.h")
-    writer.add_include("edm4hep/ReconstructedParticleData.h")
+    writer.add_include("edm4hep/MCParticleData.h")
     writer.add_include("ral/LogicalOperators.h")
+
     writer.add_namespace("k4::ral")
     writer.add_namespace("ReconstructedParticle")
+
     writer.add_getter("p", "float", Edm4hepCollection.RVEC, "momentum",
-                      momentum_property("x", "momentum", "result", "P"))
-    writer.write("h.h", "src.cpp")
+                      momentum_property("item", "momentum",  "P", "result"))
+    writer.add_getter("pt", "float", Edm4hepCollection.RVEC, "transverse momentum",
+                      momentum_property("item", "momentum",  "pt", "result"))
+    writer.add_getter("px", "float", Edm4hepCollection.RVEC, "x momentum",
+                      momentum_property("item", "momentum",  "px", "result"))
+    writer.add_getter("py", "float", Edm4hepCollection.RVEC, "y momentum",
+                      momentum_property("item", "momentum",  "py", "result"))
+    writer.add_getter("pz", "float", Edm4hepCollection.RVEC, "z momentum",
+                      momentum_property("item", "momentum",  "pz", "result"))
+    writer.add_getter("eta", "float", Edm4hepCollection.RVEC, "pseudorapidity",
+                      momentum_property("item", "momentum",  "eta", "result"))
+    writer.add_getter("rapidity", "float", Edm4hepCollection.RVEC, "rapidity",
+                      momentum_property("item", "momentum",  "Rapidity", "result"))
+    writer.add_getter("theta", "float", Edm4hepCollection.RVEC, "polar angle",
+                      momentum_property("item", "momentum",  "theta", "result"))
+    writer.add_getter("phi", "float", Edm4hepCollection.RVEC, "azimutal angle",
+                      momentum_property("item", "momentum",  "phi", "result"))
+    writer.add_getter("r", "float", Edm4hepCollection.RVEC, "distance to origin",
+                      vertex_property("item", "vertex",  "r", "result", "double"))
+    writer.add_getter("x", "float", Edm4hepCollection.RVEC, "x coordinate",
+                      vertex_property("item", "vertex",  "x", "result", "double"))
+    writer.add_getter("y", "float", Edm4hepCollection.RVEC, "y coordinate",
+                      vertex_property("item", "vertex",  "y", "result", "double"))
+    writer.add_getter("z", "float", Edm4hepCollection.RVEC, "z coordinate",
+                      vertex_property("item", "vertex",  "z", "result", "double"))
+    writer.add_getter("e", "float", Edm4hepCollection.RVEC, "energy",
+                      momentum_property("item", "momentum",  "e", "result"))
+    writer.add_getter("m", "float", Edm4hepCollection.RVEC, "mass",
+                      obj_member("item", "mass", "result"))
+    writer.add_getter("q", "float", Edm4hepCollection.RVEC, "charge",
+                      obj_member("item", "charge", "result"))
+    writer.add_getter("absq", "float", Edm4hepCollection.RVEC, "absolute charge",
+                      obj_member("item", "charge", "result", "std::abs"))
+    writer.add_getter("pdg", "int", Edm4hepCollection.RVEC, "pdg",
+                      obj_member("item", "PDG", "result"))
+    writer.add_getter("abspdg", "int", Edm4hepCollection.RVEC, "absolute pdg",
+                      obj_member("item", "PDG", "result", "std::abs"))
+
+    writer.add_mask("p", "float", Edm4hepCollection.RVEC, "momentum")
+    writer.add_mask("pt", "float", Edm4hepCollection.RVEC, "transverse momentum")
+    writer.add_mask("px", "float", Edm4hepCollection.RVEC, "x momentum")
+    writer.add_mask("py", "float", Edm4hepCollection.RVEC, "y momentum")
+    writer.add_mask("pz", "float", Edm4hepCollection.RVEC, "z momentum")
+    writer.add_mask("eta", "float", Edm4hepCollection.RVEC, "pseudorapidity")
+    writer.add_mask("rapidity", "float", Edm4hepCollection.RVEC, "rapidity")
+    writer.add_mask("theta", "float", Edm4hepCollection.RVEC, "polar angle")
+    writer.add_mask("phi", "float", Edm4hepCollection.RVEC, "azimutal angle")
+    writer.add_mask("r", "float", Edm4hepCollection.RVEC, "distance to origin")
+    writer.add_mask("x", "float", Edm4hepCollection.RVEC, "x coordinate")
+    writer.add_mask("y", "float", Edm4hepCollection.RVEC, "y coordinate")
+    writer.add_mask("z", "float", Edm4hepCollection.RVEC, "z coordinate")
+    writer.add_mask("e", "float", Edm4hepCollection.RVEC, "energy")
+    writer.add_mask("m", "float", Edm4hepCollection.RVEC, "mass")
+    writer.add_mask("q", "float", Edm4hepCollection.RVEC, "charge")
+    writer.add_mask("absq", "float", Edm4hepCollection.RVEC, "absolute charge")
+    writer.add_mask("pdg", "int", Edm4hepCollection.RVEC, "pdg")
+    writer.add_mask("abspdg", "int", Edm4hepCollection.RVEC, "absolute pdg")
+
+    writer.add_selector("p", "float", Edm4hepCollection.RVEC, "momentum")
+    writer.add_selector("pt", "float", Edm4hepCollection.RVEC, "transverse momentum")
+    writer.add_selector("px", "float", Edm4hepCollection.RVEC, "x momentum")
+    writer.add_selector("py", "float", Edm4hepCollection.RVEC, "y momentum")
+    writer.add_selector("pz", "float", Edm4hepCollection.RVEC, "z momentum")
+    writer.add_selector("eta", "float", Edm4hepCollection.RVEC, "pseudorapidity")
+    writer.add_selector("rapidity", "float", Edm4hepCollection.RVEC, "rapidity")
+    writer.add_selector("theta", "float", Edm4hepCollection.RVEC, "polar angle")
+    writer.add_selector("phi", "float", Edm4hepCollection.RVEC, "azimutal angle")
+    writer.add_selector("r", "float", Edm4hepCollection.RVEC, "distance to origin")
+    writer.add_selector("x", "float", Edm4hepCollection.RVEC, "x coordinate")
+    writer.add_selector("y", "float", Edm4hepCollection.RVEC, "y coordinate")
+    writer.add_selector("z", "float", Edm4hepCollection.RVEC, "z coordinate")
+    writer.add_selector("e", "float", Edm4hepCollection.RVEC, "energy")
+    writer.add_selector("m", "float", Edm4hepCollection.RVEC, "mass")
+    writer.add_selector("q", "float", Edm4hepCollection.RVEC, "charge")
+    writer.add_selector("absq", "float", Edm4hepCollection.RVEC, "absolute charge")
+    writer.add_selector("pdg", "int", Edm4hepCollection.RVEC, "pdg")
+    writer.add_selector("abspdg", "int", Edm4hepCollection.RVEC, "absolute pdg")
+
+    writer.write("MCParticle.h", "MCParticle.cc")
 
 
 
